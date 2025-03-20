687cc905db78d5db57aacd2d47d8b14f6448a853
K0k1 <migbumar19@gmail.com>
2025-03-19 21:01:55 -0300

    trayendo de vuelta la IA

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-#from Src.routes.deepseek_routes import router as deepseek_router
+from Src.routes.deepseek_routes import router as deepseek_router
 from Src.routes.users_routes import router as database_router
 
 app = FastAPI()
 
-#app.include_router(deepseek_router, prefix="/api")
+app.include_router(deepseek_router, prefix="/api")
 app.include_router(database_router, prefix="/api")
